7c5e1d3758a633d3783eb96d7300e078f4c40d8b
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-25 17:37:23 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash ={}
+  data.each do |c_g_l, values|
+    values.each do |diff, all_names|
+      all_names.each do |name|
+        if new_hash[name] == nil
+          new_hash[name] = {}
+        end
+        if new_hash[name][c_g_l] == nil
+          new_hash[name][c_g_l] = []
+        end
+        new_hash[name][c_g_l].push(diff.to_s)
+      end
+    end
+  end
+  new_hash
 end
