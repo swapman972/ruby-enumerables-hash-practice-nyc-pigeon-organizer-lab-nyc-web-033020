e3be4a9e71660f6c30a28963a505f601160ac55b
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-25 17:37:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
         if new_hash[name] == nil
           new_hash[name] = {}
         end
-        if new_hash[name][c_g_l] = nil
+        if new_hash[name][c_g_l] == nil
           new_hash[name][c_g_l] = []
         end
         new_hash[name][c_g_l].push(diff.to_s)
